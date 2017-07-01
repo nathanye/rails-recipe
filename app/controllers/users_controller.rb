9abce8eca2987de51a2306d6a21c0540a04386f2
nathanye <khalil54781@gmail.com>
2017-07-01 22:06:59 +0800

    implement nested form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :find_user
 
-  def edit
-    @user = current_user
+  def show
   end
 
+  def edit
+  end
+
+
   def update
-    @user = current_user
 
     if @user.update(user_params)
       flash[:notice] = "修改成功"
@@ -20,6 +23,11 @@ class UsersController < ApplicationController
   protected
 
   def user_params
-    params.require(:user).permit(:time_zone)
+    params.require(:user).permit(:time_zone, :profile_attributes => [:id, :legal_name, :birthday, :edution, :occupation, :bio, :specialty])
+  end
+
+  def find_user
+    @user = current_user
+    @user.create_profile unless @user.profile
   end
 end
